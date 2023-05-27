c680587b858ee13ee8ae96edcc1ed42dd9b3c680
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-27 13:04:51 -0700

    updating

--- missing/blog.py
+++ missing/blog.py
@@ -2,6 +2,8 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
+from werkzeug.utils import secure_filename
+import os
 
 from missing.auth import login_required
 from missing.db import get_db
@@ -12,11 +14,17 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, missed_name, since, missing_from, gender, age, call_on, addtional_info, created, finder_id,email'
+        'SELECT p.id, missed_name, since, missing_from, gender, age, call_on, addtional_info, photo_url, created, finder_id, email'
         ' FROM post p JOIN user u ON p.finder_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @bp.route('/create', methods=('GET', 'POST'))
@@ -34,27 +42,40 @@ def create():
 
         if not missed_name:
             error = 'Full Name is required.'
-        if not since:
+        elif not since:
             error = 'Date is required.'
-        if not missing_from:
+        elif not missing_from:
             error = 'Location is required.'
-        if not gender:
+        elif not gender:
             error = 'Gender is required.'
-        if not age:
+        elif not age:
             error = 'Age is required.'
-        if not call_on:
+        elif not call_on:
             error = 'Phone is required.'
-        if not addtional_info:
+        elif not addtional_info:
             error = 'Full information is required.'
+
+        # handle photo_url upload
+        if 'photo_url' not in request.files:
+            error = 'photo_url is required.'
+        else:
+            file = request.files['photo_url']
+            if file.filename == '':
+                error = 'photo_url is required.'
+            elif not allowed_file(file.filename):
+                error = 'Invalid file type. Only PNG, JPG, JPEG, and GIF are allowed.'
+            else:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (missed_name, since, missing_from, gender, age, call_on, addtional_info, finder_id)'
-                ' VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                (missed_name, since, missing_from, gender, age, call_on, addtional_info, g.user['id'])
+                'INSERT INTO post (missed_name, since, missing_from, gender, age, call_on, addtional_info, photo_url finder_id)'
+                ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                (missed_name, since, missing_from, gender, age, call_on, addtional_info, filename, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
