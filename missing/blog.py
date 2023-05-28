38908ab70a959286b060c517e7209f9ed03018a1
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-28 04:51:06 -0700

    update

--- missing/blog.py
+++ missing/blog.py
@@ -1,17 +1,21 @@
+import os
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
-import os
 
 from missing.auth import login_required
 from missing.db import get_db
 
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 bp = Blueprint('blog', __name__)
+
 
 @bp.route('/')
 def index():
@@ -22,7 +26,6 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
-
 
 
 def allowed_file(filename):
@@ -86,3 +89,75 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+def get_post(id, check_finder=True):
+    post = get_db().execute(
+        'SELECT p.id, missed_name, since, missing_from,  gender, age,  call_on, addtional_info, created, finder_id, email'
+        ' FROM post p JOIN user u ON p.finder_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_finder and post['finder_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        missed_name = request.form['missed_name']
+        since = request.form['since']
+        missing_from = request.form['missing_from']
+        gender = request.form['gender']
+        age = request.form['age']
+        call_on = request.form['call_on']
+        addtional_info = request.form['addtional_info']
+        error = None
+
+        if not missed_name:
+            error = 'Full Name is required.'
+        elif not since:
+            error = 'Date is required.'
+        elif not missing_from:
+            error = 'Location is required.'
+        elif not gender:
+            error = 'Gender is required.'
+        elif not age:
+            error = 'Age is required.'
+        elif not call_on:
+            error = 'Phone is required.'
+        elif not addtional_info:
+            error = 'Full information is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET missed_name = ?, since = ?, missing_from = ?, gender = ?, age = ?, call_on = ?, addtional_info = ?'
+                ' WHERE id = ?',
+                (missed_name, since, missing_from,  gender, age,  call_on, addtional_info, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
