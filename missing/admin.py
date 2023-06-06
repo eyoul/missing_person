0b5bf0782f03a5badb17f3984b99418c5cce8fa6
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-06-06 16:10:49 -0700

    new update

--- missing/admin.py
+++ missing/admin.py
@@ -7,12 +7,9 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from missing.db import get_db
 
+
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
-
-@bp.route('/login', methods=('GET', 'POST'))
-def login():
-    return render_template('auth/login.html')
 
 
 def login_required_admin(role=None):
@@ -32,7 +29,7 @@ def login_required_admin(role=None):
 @bp.route('/admin')
 @login_required_admin(role='admin')
 def admin():
-    return render_template('admin.html')
+    return render_template('dashboard.html')
 
 
 @bp.route('/dashboard')
@@ -45,6 +42,22 @@ def dashboard():
     return render_template('admin/dashboard.html', total_reports=total_reports,
                            active_reports=active_reports, resolved_reports=resolved_reports)
 
+def get_total_reports():
+    db = get_db()
+    result = db.execute('SELECT COUNT(*) FROM post').fetchone()
+    return result[0]
+
+
+def get_active_reports():
+    db = get_db()
+    result = db.execute('SELECT COUNT(*) FROM post WHERE status = "active"').fetchone()
+    return result[0]
+
+
+def get_resolved_reports():
+    db = get_db()
+    result = db.execute('SELECT COUNT(*) FROM post WHERE status = "resolved"').fetchone()
+    return result[0]
 
 @bp.route('/users')
 def users():
@@ -53,12 +66,16 @@ def users():
 
     return render_template('admin/users.html', users=users)
 
+def get_all_users():
+    db = get_db()
+    users = db.execute('SELECT * FROM user').fetchall()
+    return users
+
 
 @bp.route('/reports')
 def reports():
-    # Get a list of all reports
-    reports = get_all_reports()
-
+    db = get_db()
+    reports = db.execute('SELECT * FROM post').fetchall()
     return render_template('admin/reports.html', reports=reports)
 
 
@@ -72,3 +89,20 @@ def settings():
     return render_template('admin/settings.html', app_name=app_name,
                            default_location=default_location,
                            max_reports_per_user=max_reports_per_user)
+
+def get_app_name():
+    db = get_db()
+    result = db.execute('SELECT value FROM settings WHERE name = "post"').fetchone()
+    return result[0]
+
+
+def get_default_location():
+    db = get_db()
+    result = db.execute('SELECT value FROM settings WHERE name = "default_location"').fetchone()
+    return result[0]
+
+
+def get_max_reports_per_user():
+    db = get_db()
+    result = db.execute('SELECT value FROM settings WHERE name = "max_reports_per_user"').fetchone()
+    return result[0]
